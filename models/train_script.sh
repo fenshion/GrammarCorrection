6483a05fa30bb3332d2838c579d315cc335c26ff
fenshion <39155834+fenshion@users.noreply.github.com>
2019-01-18 14:46:38 +0800

    Update train_script.sh

--- models/train_script.sh
+++ models/train_script.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-source ../paths.sh
+. ./paths.sh
 
 ROOT_DIR=$BASE_DIR
 
@@ -13,13 +13,13 @@ chmod u+x $ROOT_DIR/models/predict.py
 
 $ROOT_DIR/models/prepare_data.py --text_A=$ROOT_DIR/data/final-train/final-train.tok.src \
                                 --text_B=$ROOT_DIR/data/final-train/final-train.tok.trg \
-                                --out_file=$ROOT_DIR/data/trg_src_prepped.h5
+                                --cache=$ROOT_DIR/data/trg_src_prepped.h5
 
 
 mkdir -p $ROOT_DIR/models/weights
-$ROOT_DIR/models/train.py --dataset=$ROOT_DIR/data/trg_src_prepped.h5 \
+$ROOT_DIR/models/train.py --cahce=$ROOT_DIR/data/trg_src_prepped.h5 \
                         --weights_path=$ROOT_DIR/train/weights/KerasAttentionNMT.h5
 
 
-$ROOT_DIR/models/predict.py --dataset=$ROOT_DIR/data/trg_src_prepped.h5 \
+$ROOT_DIR/models/predict.py --cahce_dir=$ROOT_DIR/data/trg_src_prepped.h5 \
                             --weights_path=$ROOT_DIR/weights/KerasAttentionNMT.h5
